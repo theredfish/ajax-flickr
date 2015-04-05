7911b42879a0588cb4094b3dcf310b608d86f5fd
julian <did.julian@gmail.com>
2015-04-05 04:25:08 +0200

    amazing gallery with justified gallery plugin

--- script/get-photos.js
+++ script/get-photos.js
@@ -1,49 +1,72 @@
 $(document).ready(function() 
 {
-    var page = 0;
-    // Get the number of images to show
+    // Initialization
+    var page = 1
+    var selectedNumber = parseInt($("input[type='radio']:checked").val()) 
+    $(".precedent").addClass("disabled")
+    $(".suivant").addClass("disabled")
+    $(".endButtons").hide()
 
-    var selectedNumber = parseInt($("input[type='radio']:checked").val()); 
-
-    $("#precedent").hide();
-    $("#suivant").hide();
-    $("#detail").hide();
-
-    $( "#submit-photos-pagination" ).on("click", function() {
+    // Research button click
+    $("#submit-photos-pagination").on("click", function() {
+        if( $("#commune").val() == ""){
+            alert("Vous devez saisir au moins un mot clé")
+            $("#commune").focus()
+            return false
+        }
         page = 1
         getDataPaginationMode(page);
     });
 
-    $("#precedent").on("click", function(){
+    // Previous button click
+    $(".precedent").on("click", function(){
         if (page != 1)
-            page--;
-        getDataPaginationMode(page);
+            page--
+        watchPreviousButton(page)
+        getDataPaginationMode(page)
     });
 
-    $("#suivant").on("click", function(){
+    // Next button click
+    $(".suivant").on("click", function(){
         page++;
+        watchPreviousButton(page)
         getDataPaginationMode(page);
     });
 
     // When a change occurs, update the number of images to display
     $("input[type='radio']").on("change",function() {
         selectedNumber = parseInt($("input[type='radio']:checked").val());
+        console.log(selectedNumber)
     })
+
+    // This function change class of previous button relative with page
+    function watchPreviousButton(page){
+        if (page == 1)
+            $(".precedent").addClass("disabled")
+        else
+            $(".precedent").removeClass("disabled")
+
+    }
 
     // Function to get images data from Flickr API in pagination mode
     function getDataPaginationMode(atPage)
     {
+        $(".endButtons").show()
+        // Set next button available
+        $(".suivant").removeClass("disabled")
         // Clear the div of images when a new research is done
         $("#showImagesPaginationMode").empty();
+        // Set the div as justified gallery
+        $("#showImagesPaginationMode").justifiedGallery({
+            rowHeight : 300,
+            margins : 3
+        });
         $.ajax({
             type: "GET",
             dataType: "json",
-            //https://www.flickr.com/services/feeds/docs/photos_public/
             url: "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=0ec1c8867eeca73bca63ed9b7365ad5b&format=json&jsoncallback=?",
             data: "tags=" + $("#commune").val()+"&per_page="+selectedNumber+"&page="+atPage,
             success: function(data) {
-                $("#precedent").show();
-                $("#suivant").show();
                 $.each(data.photos.photo, function(i, image) 
                 {
                     // Show only the good number of images
@@ -54,78 +77,93 @@ $(document).ready(function()
                     getImagesPaginationMode(image);
                     
                 })
-                
+            //FAILURE TODO 
+            },
+            error : function(){
+                alert("error")
             }
         });
     }
     
+    getDataCarouselMode(1)
     // Function to get data images for the carosuel mode
     function getDataCarouselMode(atPage)
     {
-        // Clear the div of images when a new research is done
-        $("#showImagesCarouselMode").empty();
-        $.getJSON("test/agile_carousel_data.php", function(data) {
-        $(document).ready(function(){
-            $("#showImagesCarouselMode").agile_carousel({
-                carousel_data: data,
-                carousel_outer_height: 228,
-                carousel_height: 228,
-                slide_height: 230,
-                carousel_outer_width: 480,
-                slide_width: 480,
-                transition_time: 300,
-                timer: 4000,
-                continuous_scrolling: true,
-                control_set_1: "numbered_buttons",
-                no_control_set: "hover_previous_button,hover_next_button"
-            });
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=0ec1c8867eeca73bca63ed9b7365ad5b&format=json&jsoncallback=?",
+            data: "tags=nantes&per_page="+selectedNumber+"&page="+atPage,
+            success: function(data) {
+                $.each(data.photos.photo, function(i, image) 
+                {
+                    // Show only the good number of images
+                    if(i == selectedNumber)
+                        return false;
+
+                    // Call getImages function to delegate the responsability
+                    getImagesCarouselMode(image);
+                    
+                })
+            },
+            error : function(){
+                alert("error")
+            }
         });
-    });
+        
     }
 
     // Function to show image
     function getImagesPaginationMode(image) {
-        var link = "http://farm"+image.farm+".staticflickr.com/"+image.server+"/"+image.id+"_"+image.secret+"_m.jpg";
-        $("#showImagesPaginationMode").append('<div class="col-lg-12 col-md-12 col-xs-12"><a href="#"><img onmouseover="getImageInformation('+image.id+')" id="moreInformation" src="'+link+'" alt="Generic placeholder"></a></div>')
+        var link = "http://farm"+image.farm+".staticflickr.com/"+image.server+"/"+image.id+"_"+image.secret+"_z.jpg";
+        //onmouseover="getImageInformation('+image.id+')" 
+        $("#showImagesPaginationMode").append('<a href="#"><img src="'+link+'" alt=""></a>')
+        $("#showImagesPaginationMode").justifiedGallery().on('jg.complete', function (e) {
+            return true;
+        });
+    }
+
+    // Function to show image
+    function getImagesCarouselMode(image) {
+        var link = "http://farm"+image.farm+".staticflickr.com/"+image.server+"/"+image.id+"_"+image.secret+"_z.jpg";
+        $(".jcarousel-list").append('<li><img src="'+link+'" width="600" height="400" alt=""></li>')
+        $('.jcarousel').jcarousel('reload');
+        /*<div class="col-lg-12 col-md-12 col-xs-12"><a href="#"><img id="moreInformation" src="'+link+'" alt="Generic placeholder"></a></div>*/
     }
 
     // Function to get specific information about an image (ex : title, author, date, ...)
-    /*$("#moreInformation").on("click", function()
-    {
-        $("#detail").show();*/
-        function getImageInformation(id)
-        { 
-            $.ajax(
+    function getImageInformation(id)
+    { 
+        $.ajax(
+        {
+            type: "POST",
+            dataType: "json",
+            url: "https://api.flickr.com/services/rest/?&method=flickr.photos.getInfo&api_key=0ec1c8867eeca73bca63ed9b7365ad5b&format=json&jsoncallback=?",
+            data: "photo_id="+id,
+            success: function(data) 
             {
-                type: "POST",
-                dataType: "json",
-                url: "https://api.flickr.com/services/rest/?&method=flickr.photos.getInfo&api_key=0ec1c8867eeca73bca63ed9b7365ad5b&format=json&jsoncallback=?",
-                data: "photo_id="+id,
-                success: function(data) 
+                var date = data.photo.dates.posted;
+
+                var formattedTime = timeConverter(date)
+
+                // alert(data.photo, data.photo.title._content, data.photo.owner.username, data.photo.dates.posted);
+                $("#detail").show();
+                if (data.photo.title._content == '') 
                 {
-                    var date = data.photo.dates.posted;
-
-                    var formattedTime = timeConverter(date)
-
-                    // alert(data.photo, data.photo.title._content, data.photo.owner.username, data.photo.dates.posted);
-                    $("#detail").show();
-                    if (data.photo.title._content == '') 
-                    {
-                        $("#detailTitre").text("[Photo sans titre...]");
-                    }
-                    else
-                    {
-                        $("#detailTitre").text(data.photo.title._content);
-                    }
-                    $("#detailAuteur").text(data.photo.owner.username);
-                    $("#detailDate").text(formattedTime);
+                    $("#detailTitre").text("[Photo sans titre...]");
                 }
-            });
-        }
-    /*});*/
+                else
+                {
+                    $("#detailTitre").text(data.photo.title._content);
+                }
+                $("#detailAuteur").text(data.photo.owner.username);
+                $("#detailDate").text(formattedTime);
+            }
+        });
+    }
     
-
-
+    // Function to convert a date (unix time stamp) in a readable format by humans
+    // This function will no longer work on 19 January 2038 :=)
     function timeConverter(UNIX_timestamp)
     {
         var a = new Date(UNIX_timestamp*1000);
@@ -141,3 +179,4 @@ $(document).ready(function()
         return time;
     }
 });
+
